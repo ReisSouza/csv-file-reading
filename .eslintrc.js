4cd7b6775233fe28b703fb3fca1aa60062cca95c
reissouza <reis.souza@outlook.com.br>
2022-07-24 17:55:03 -0300

    build: :heavy_plus_sign: add dependences and config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
